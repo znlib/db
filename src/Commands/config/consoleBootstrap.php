c3f9ce212427f04fb23c2b6233f9335c3014483f
Vitaliy <theyamshikov@yandex.ru>
2021-06-26 19:00:53 +0600

    upd

--- src/Commands/config/consoleBootstrap.php
+++ src/Commands/config/consoleBootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Psr\Container\ContainerInterface;
+use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
 use ZnCore\Base\Libs\App\Kernel;
 use ZnCore\Base\Libs\App\Loaders\BundleLoader;
 use ZnCore\Base\Libs\DotEnv\DotEnv;
@@ -14,12 +15,20 @@ DotEnv::init();
 $kernel = new Kernel('console');
 $container = $kernel->getContainer();
 
-$container->singleton(EntityManagerInterface::class, function (ContainerInterface $container) {
+$containerConfigurator = ContainerHelper::getContainerConfiguratorByContainer($container);
+$containerConfigurator->singleton(EntityManagerInterface::class, function (ContainerInterface $container) {
+    return EntityManager::getInstance($container);
+});
+$containerConfigurator->singleton(Manager::class, function () {
+    return ManagerFactory::createManagerFromEnv();
+});
+
+/*$container->singleton(EntityManagerInterface::class, function (ContainerInterface $container) {
     return EntityManager::getInstance($container);
 });
 $container->singleton(Manager::class, function () {
     return ManagerFactory::createManagerFromEnv();
-});
+});*/
 
 $bundleLoader = new BundleLoader([], ['i18next', 'container', 'console', 'migration']);
 $bundleLoader->addBundles(include __DIR__ . '/bundle.php');
