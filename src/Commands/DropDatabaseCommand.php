68983e8f6fc3944f48b1d0ba3e2fddd5ccb9f428
Vitaliy <theyamshikov@yandex.ru>
2021-08-06 16:11:57 +0600

    upd

--- src/Commands/DropDatabaseCommand.php
+++ src/Commands/DropDatabaseCommand.php
@@ -14,15 +14,19 @@ use ZnLib\Console\Symfony4\Helpers\OutputHepler;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use ZnLib\Fixture\Domain\Services\FixtureService;
+use ZnSandbox\Sandbox\Generator\Domain\Entities\TableEntity;
+use ZnSandbox\Sandbox\Generator\Domain\Repositories\Eloquent\SchemaRepository;
 
 class DropDatabaseCommand extends Command
 {
     protected static $defaultName = 'db:database:drop';
     private $capsule;
+    private $schemaRepository;
 
-    public function __construct(?string $name = null)
+    public function __construct(?string $name = null, SchemaRepository $schemaRepository)
     {
         $this->capsule = ManagerFactory::createManagerFromEnv();
+        $this->schemaRepository = $schemaRepository;
         parent::__construct($name);
     }
 
@@ -46,25 +50,40 @@ class DropDatabaseCommand extends Command
         $connections = DbFacade::getConfigFromEnv();
         foreach ($connections as $connectionName => $connection) {
             $conn = $this->capsule->getConnection($connectionName);
-            $tableList = $conn->select('
+            $tableList = $this->schemaRepository->allTables();
+            //dd($tableList);
+            
+            /*$tableList = $conn->select('
                 SELECT *
                 FROM pg_catalog.pg_tables
-                WHERE schemaname != \'pg_catalog\' AND schemaname != \'information_schema\';');
+                WHERE schemaname != \'pg_catalog\' AND schemaname != \'information_schema\';');*/
             $tables = [];
             $schemas = [];
-            foreach ($tableList as $tableRecord) {
-                $tableName = $tableRecord->schemaname . '.' . $tableRecord->tablename;
+            foreach ($tableList as $tableEntity) {
+                $tableName = $tableEntity->getName();
+                if($tableEntity->getSchemaName()) {
+                    $tableName = $tableEntity->getSchemaName() . '.' . $tableName;
+                }
+                //dd($tableEntity->getSchemaName());
+                
                 $tables[] = $tableName;
-                if($tableRecord->schemaname != 'public') {
-                    $schemas[] = $tableRecord->schemaname;
+                if($tableEntity->getSchemaName() && $tableEntity->getSchemaName() != 'public') {
+                    $schemas[] = $tableEntity->getSchemaName();
                 }
             }
+//            dd($tables);
             if(empty($tables)) {
                 $output->writeln(['', '<fg=yellow>Not found tables!</>', '']);
             } else {
+                
+                /*foreach ($tables as $t) {
+                    $this->capsule->getSchemaByTableName($t)->dropIfExists($t);
+                }*/
+                
                 $sql = 'DROP TABLE ' . implode(', ', $tables);
                 $conn->statement($sql);
             }
+            
             $schemaList = $conn->select('select nspname from pg_catalog.pg_namespace;');
             foreach ($schemaList as $schemaRecord) {
                 if(strpos($schemaRecord->nspname, 'pg_') === false && !in_array($schemaRecord->nspname, ['information_schema', 'public'])) {
