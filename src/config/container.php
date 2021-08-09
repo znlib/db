39adf3271857c2706dec654c2de6751e05f45453
Vitaliy <theyamshikov@yandex.ru>
2021-08-09 11:22:34 +0600

    upd

--- src/config/container.php
+++ src/config/container.php
@@ -9,9 +9,15 @@ use ZnLib\Db\Factories\ManagerFactory;
 return [
     'definitions' => [],
     'singletons' => [
-        EntityManagerInterface::class => EntityManager::class,
+        /*EntityManagerInterface::class => EntityManager::class,
         EntityManager::class => function (ContainerInterface $container) {
             return EntityManager::getInstance($container);
+        },*/
+        EntityManagerInterface::class => function (ContainerInterface $container) {
+            $em = EntityManager::getInstance($container);
+//            $eloquentOrm = $container->get(EloquentOrm::class);
+//            $em->addOrm($eloquentOrm);
+            return $em;
         },
         Manager::class => function () {
             return ManagerFactory::createManagerFromEnv();
