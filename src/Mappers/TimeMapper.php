9198ca36b7e413183fca916d3cbbb30e755fb425
Vitaliy <theyamshikov@yandex.ru>
2021-08-10 15:36:52 +0600

    upd

---
+++ src/Mappers/TimeMapper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ZnLib\Db\Mappers;
+
+use ZnCore\Base\Interfaces\EncoderInterface;
+
+class TimeMapper implements EncoderInterface
+{
+
+    public $format = 'Y-m-d H:i:s';
+    private $attributes;
+
+    public function __construct(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function encode($data)
+    {
+        foreach ($this->attributes as $attribute) {
+//            $data[$attribute] = $time->format($this->format);
+//            $data[$attribute] = json_encode($data[$attribute], JSON_UNESCAPED_UNICODE);
+        }
+
+        return $data;
+    }
+
+    public function decode($row)
+    {
+        foreach ($this->attributes as $attribute) {
+            $row[$attribute] = new \DateTime($row[$attribute]);
+        }
+        return $row;
+    }
+}
