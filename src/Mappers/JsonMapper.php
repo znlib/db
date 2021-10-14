c08210a07c2588439485250511ec0831e15669da
Vitaliy <theyamshikov@yandex.ru>
2021-10-14 11:04:03 +0600

    upd

--- src/Mappers/JsonMapper.php
+++ src/Mappers/JsonMapper.php
@@ -19,14 +19,16 @@ class JsonMapper implements EncoderInterface
         foreach ($this->attributes as $attribute) {
             $data[$attribute] = json_encode($data[$attribute], JSON_UNESCAPED_UNICODE);
         }
-
         return $data;
     }
 
     public function decode($row)
     {
         foreach ($this->attributes as $attribute) {
-            $row[$attribute] = json_decode($row[$attribute], JSON_OBJECT_AS_ARRAY);
+            $value = $row[$attribute] ?? null;
+            if($value) {
+                $row[$attribute] = json_decode($row[$attribute], JSON_OBJECT_AS_ARRAY);
+            }
         }
         return $row;
     }
